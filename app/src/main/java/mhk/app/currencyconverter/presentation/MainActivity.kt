40933140d56c849195525a5cc9d83f29c6d8a489
Humza <alfaios123@gmail.com>
2023-01-07 20:44:33 +0500

    Home Screen UI Done Fragment Nav by NavGraph Done

--- app/src/main/java/mhk/app/currencyconverter/presentation/MainActivity.kt
+++ app/src/main/java/mhk/app/currencyconverter/presentation/MainActivity.kt
@@ -3,10 +3,18 @@ package mhk.app.currencyconverter.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mhk.app.currencyconverter.R
+import mhk.app.currencyconverter.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
---
+++ app/src/main/java/mhk/app/currencyconverter/presentation/fragments/HomeFragment.kt
@@ -0,0 +1,39 @@
+package mhk.app.currencyconverter.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import mhk.app.currencyconverter.R
+import mhk.app.currencyconverter.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDetail.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
+        }
+    }
+
+
+}
